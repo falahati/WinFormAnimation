a8ab5f6326292d4f8c7a5d6825b98203701cf42a
Soroush Falahati <s_falahati@hotmail.com>
2016-05-13 14:46:30 +0430

    New animation functions added
    * Quintic
    * Quartic
    * Quadratic
    * Circular
    * Exponential
    * Sinusoidal

--- WinFormAnimation/Functions.cs
+++ WinFormAnimation/Functions.cs
@@ -148,8 +148,143 @@ namespace WinFormAnimation
         public static float Liner(float t, float b, float c, float d)
         {
             return (c * t / d) + b;
+        public static float CircularEaseInOut(float t, float b, float c, float d)
+        {
+            t /= d/2;
+            if (t < 1)
+            {
+                return (float) (-c/2*(Math.Sqrt(1 - t*t) - 1) + b);
+            }
+            t -= 2;
+            return (float) (c/2*(Math.Sqrt(1 - t*t) + 1) + b);
+        }
+
+
+        public static float QuadraticEaseIn(float t, float b, float c, float d)
+        {
+            t /= d;
+            return c*t*t + b;
+        }
+
+
+        public static float QuadraticEaseOut(float t, float b, float c, float d)
+        {
+            t /= d;
+            return -c*t*(t - 2) + b;
+        }
+
+
+        public static float QuadraticEaseInOut(float t, float b, float c, float d)
+        {
+            t /= d/2;
+            if (t < 1) return c/2*t*t + b;
+            t--;
+            return -c/2*(t*(t - 2) - 1) + b;
+        }
+
+
+        public static float QuarticEaseIn(float t, float b, float c, float d)
+        {
+            t /= d;
+            return c*t*t*t*t + b;
+        }
+
+
+        public static float QuarticEaseOut(float t, float b, float c, float d)
+        {
+            t /= d;
+            t--;
+            return -c*(t*t*t*t - 1) + b;
+        }
+
+
+        public static float QuarticEaseInOut(float t, float b, float c, float d)
+        {
+            t /= d/2;
+            if (t < 1) return c/2*t*t*t*t + b;
+            t -= 2;
+            return -c/2*(t*t*t*t - 2) + b;
+        }
+
+
+        public static float QuinticEaseIn(float t, float b, float c, float d)
+        {
+            t /= d;
+            return c*t*t*t*t*t + b;
+        }
+
+
+        public static float QuinticEaseOut(float t, float b, float c, float d)
+        {
+            t /= d;
+            t--;
+            return c*(t*t*t*t*t + 1) + b;
+        }
+
+
+        public static float SinusoidalEaseIn(float t, float b, float c, float d)
+        {
+            return (float) (-c*Math.Cos(t/d*(Math.PI/2)) + c + b);
+        }
+
+
+        public static float SinusoidalEaseOut(float t, float b, float c, float d)
+        {
+            return (float) (c*Math.Sin(t/d*(Math.PI/2)) + b);
+        }
+
+
+        public static float SinusoidalEaseInOut(float t, float b, float c, float d)
+        {
+            return (float) (-c/2*(Math.Cos(Math.PI*t/d) - 1) + b);
+        }
+
+
+        public static float ExponentialEaseIn(float t, float b, float c, float d)
+        {
+            return (float) (c*Math.Pow(2, 10*(t/d - 1)) + b);
+        }
+
+        public static float ExponentialEaseOut(float t, float b, float c, float d)
+        {
+            return (float) (c*(-Math.Pow(2, -10*t/d) + 1) + b);
+        }
+
+
+        public static float ExponentialEaseInOut(float t, float b, float c, float d)
+        {
+            t /= d/2;
+            if (t < 1)
+            {
+                return (float) (c/2*Math.Pow(2, 10*(t - 1)) + b);
+            }
+            t--;
+            return (float) (c/2*(-Math.Pow(2, -10*t) + 2) + b);
+        }
+
+
+        public static float CircularEaseIn(float t, float b, float c, float d)
+        {
+            t /= d;
+            return (float) (-c*(Math.Sqrt(1 - t*t) - 1) + b);
         }
 
         #endregion
+
+        public static float CircularEaseOut(float t, float b, float c, float d)
+        {
+            t /= d;
+            t--;
+            return (float) (c*Math.Sqrt(1 - t*t) + b);
+        }
+
+
+        public static float QuinticEaseInOut(float t, float b, float c, float d)
+        {
+            t /= d/2;
+            if (t < 1) return c/2*t*t*t*t*t + b;
+            t -= 2;
+            return c/2*(t*t*t*t*t + 2) + b;
+        }
     }
 }
