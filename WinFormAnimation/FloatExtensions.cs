1bca6c530ab00cde3355be46d1a51495f6b2f2a0
Soroush Falahati <s_falahati@hotmail.com>
2016-05-15 02:17:08 +0430

    Extension methods added for easier converting of values to Float2D and Float3D

---
+++ WinFormAnimation/FloatExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+
+namespace WinFormAnimation
+{
+    /// <summary>
+    ///     Contains public extension methods about Float2D and Fload3D classes
+    /// </summary>
+    public static class FloatExtensions
+    {
+        /// <summary>
+        ///     Creates and returns a new instance of the <see cref="Float2D" /> class from this instance
+        /// </summary>
+        /// <param name="point">The object to create the <see cref="Float2D" /> instance from</param>
+        /// <returns>The newly created <see cref="Float2D" /> instance</returns>
+        public static Float2D ToFloat2D(this Point point)
+        {
+            return Float2D.FromPoint(point);
+        }
+
+        /// <summary>
+        ///     Creates and returns a new instance of the <see cref="Float2D" /> class from this instance
+        /// </summary>
+        /// <param name="point">The object to create the <see cref="Float2D" /> instance from</param>
+        /// <returns>The newly created <see cref="Float2D" /> instance</returns>
+        public static Float2D ToFloat2D(this PointF point)
+        {
+            return Float2D.FromPoint(point);
+        }
+
+        /// <summary>
+        ///     Creates and returns a new instance of the <see cref="Float2D" /> class from this instance
+        /// </summary>
+        /// <param name="size">The object to create the <see cref="Float2D" /> instance from</param>
+        /// <returns>The newly created <see cref="Float2D" /> instance</returns>
+        public static Float2D ToFloat2D(this Size size)
+        {
+            return Float2D.FromSize(size);
+        }
+
+        /// <summary>
+        ///     Creates and returns a new instance of the <see cref="Float2D" /> class from this instance
+        /// </summary>
+        /// <param name="size">The object to create the <see cref="Float2D" /> instance from</param>
+        /// <returns>The newly created <see cref="Float2D" /> instance</returns>
+        public static Float2D ToFloat2D(this SizeF size)
+        {
+            return Float2D.FromSize(size);
+        }
+
+        /// <summary>
+        ///     Creates and returns a new instance of the <see cref="Float3D" /> class from this instance
+        /// </summary>
+        /// <param name="color">The object to create the <see cref="Float3D" /> instance from</param>
+        /// <returns>The newly created <see cref="Float3D" /> instance</returns>
+        public static Float3D ToFloat3D(this Color color)
+        {
+            return Float3D.FromColor(color);
+        }
+    }
+}
