a7e04b4bbace10421e0eb72c53b2d1af5a763c4b
s_falahati <s_falahati@hotmail.com>
2017-03-15 00:48:53 +0330

    An Enum for known animation functions added so it is much easier now to use them and drops the need for delegate serialization in most scenarios

--- WinFormAnimation/AnimationFunctions.cs
+++ WinFormAnimation/AnimationFunctions.cs
@@ -27,6 +27,65 @@ namespace WinFormAnimation
         /// </returns>
         public delegate float Function(float time, float beginningValue, float changeInValue, float duration);
 
+        /// <summary>
+        ///     Returns a function delegate based on the the passed known animation function
+        /// </summary>
+        /// <param name="knownFunction">The animation function</param>
+        /// <returns>Animation fucntion delegate</returns>
+        public static Function FromKnown(KnownAnimationFunctions knownFunction)
+        {
+            switch (knownFunction)
+            {
+                case KnownAnimationFunctions.CubicEaseIn:
+                    return CubicEaseIn;
+                case KnownAnimationFunctions.CubicEaseInOut:
+                    return CubicEaseInOut;
+                case KnownAnimationFunctions.CubicEaseOut:
+                    return CubicEaseOut;
+                case KnownAnimationFunctions.Liner:
+                    return Liner;
+                case KnownAnimationFunctions.CircularEaseInOut:
+                    return CircularEaseInOut;
+                case KnownAnimationFunctions.CircularEaseIn:
+                    return CircularEaseIn;
+                case KnownAnimationFunctions.CircularEaseOut:
+                    return CircularEaseOut;
+                case KnownAnimationFunctions.QuadraticEaseIn:
+                    return QuadraticEaseIn;
+                case KnownAnimationFunctions.QuadraticEaseOut:
+                    return QuadraticEaseOut;
+                case KnownAnimationFunctions.QuadraticEaseInOut:
+                    return QuadraticEaseInOut;
+                case KnownAnimationFunctions.QuarticEaseIn:
+                    return QuarticEaseIn;
+                case KnownAnimationFunctions.QuarticEaseOut:
+                    return QuarticEaseOut;
+                case KnownAnimationFunctions.QuarticEaseInOut:
+                    return QuarticEaseInOut;
+                case KnownAnimationFunctions.QuinticEaseInOut:
+                    return QuinticEaseInOut;
+                case KnownAnimationFunctions.QuinticEaseIn:
+                    return QuinticEaseIn;
+                case KnownAnimationFunctions.QuinticEaseOut:
+                    return QuinticEaseOut;
+                case KnownAnimationFunctions.SinusoidalEaseIn:
+                    return SinusoidalEaseIn;
+                case KnownAnimationFunctions.SinusoidalEaseOut:
+                    return SinusoidalEaseOut;
+                case KnownAnimationFunctions.SinusoidalEaseInOut:
+                    return SinusoidalEaseInOut;
+                case KnownAnimationFunctions.ExponentialEaseIn:
+                    return ExponentialEaseIn;
+                case KnownAnimationFunctions.ExponentialEaseOut:
+                    return ExponentialEaseOut;
+                case KnownAnimationFunctions.ExponentialEaseInOut:
+                    return ExponentialEaseInOut;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(knownFunction), knownFunction,
+                        "The passed animation function is unknown.");
+            }
+        }
+
 
         /// <summary>
         ///     The cubic ease-in animation function.
@@ -49,7 +108,7 @@ namespace WinFormAnimation
         public static float CubicEaseIn(float t, float b, float c, float d)
         {
             t /= d;
-            return (c*t*t*t) + b;
+            return c*t*t*t + b;
         }
 
         /// <summary>
@@ -74,12 +133,10 @@ namespace WinFormAnimation
         {
             t /= d/2;
             if (t < 1)
-            {
                 return c/2*t*t*t + b;
-            }
 
             t -= 2;
-            return (c/2*(t*t*t + 2)) + b;
+            return c/2*(t*t*t + 2) + b;
         }
 
         /// <summary>
@@ -104,7 +161,7 @@ namespace WinFormAnimation
         {
             t /= d;
             t--;
-            return (c*((t*t*t) + 1)) + b;
+            return c*(t*t*t + 1) + b;
         }
 
         /// <summary>
@@ -127,7 +184,7 @@ namespace WinFormAnimation
         /// </returns>
         public static float Liner(float t, float b, float c, float d)
         {
-            return (c*t/d) + b;
+            return c*t/d + b;
         }
 
         /// <summary>
@@ -152,9 +209,7 @@ namespace WinFormAnimation
         {
             t /= d/2;
             if (t < 1)
-            {
                 return (float) (-c/2*(Math.Sqrt(1 - t*t) - 1) + b);
-            }
             t -= 2;
             return (float) (c/2*(Math.Sqrt(1 - t*t) + 1) + b);
         }
@@ -575,9 +630,7 @@ namespace WinFormAnimation
         {
             t /= d/2;
             if (t < 1)
-            {
                 return (float) (c/2*Math.Pow(2, 10*(t - 1)) + b);
-            }
             t--;
             return (float) (c/2*(-Math.Pow(2, -10*t) + 2) + b);
         }
---
+++ WinFormAnimation/KnownAnimationFunctions.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormAnimation
+{
+    /// <summary>
+    ///     Contains a list of all known animation functions
+    /// </summary>
+    public enum KnownAnimationFunctions
+    {
+        /// <summary>
+        ///     No known animation function
+        /// </summary>
+        None,
+
+        /// <summary>
+        ///     The cubic ease-in animation function.
+        /// </summary>
+        CubicEaseIn,
+
+        /// <summary>
+        ///     The cubic ease-in and ease-out animation function.
+        /// </summary>
+        CubicEaseInOut,
+
+        /// <summary>
+        ///     The cubic ease-out animation function.
+        /// </summary>
+        CubicEaseOut,
+
+        /// <summary>
+        ///     The liner animation function.
+        /// </summary>
+        Liner,
+
+        /// <summary>
+        ///     The circular ease-in and ease-out animation function.
+        /// </summary>
+        CircularEaseInOut,
+
+        /// <summary>
+        ///     The circular ease-in animation function.
+        /// </summary>
+        CircularEaseIn,
+
+        /// <summary>
+        ///     The circular ease-out animation function.
+        /// </summary>
+        CircularEaseOut,
+
+        /// <summary>
+        ///     The quadratic ease-in animation function.
+        /// </summary>
+        QuadraticEaseIn,
+
+        /// <summary>
+        ///     The quadratic ease-out animation function.
+        /// </summary>
+        QuadraticEaseOut,
+
+        /// <summary>
+        ///     The quadratic ease-in and ease-out animation function.
+        /// </summary>
+        QuadraticEaseInOut,
+
+        /// <summary>
+        ///     The quartic ease-in animation function.
+        /// </summary>
+        QuarticEaseIn,
+
+        /// <summary>
+        ///     The quartic ease-out animation function.
+        /// </summary>
+        QuarticEaseOut,
+
+        /// <summary>
+        ///     The quartic ease-in and ease-out animation function.
+        /// </summary>
+        QuarticEaseInOut,
+
+        /// <summary>
+        ///     The quintic ease-in and ease-out animation function.
+        /// </summary>
+        QuinticEaseInOut,
+
+        /// <summary>
+        ///     The quintic ease-in animation function.
+        /// </summary>
+        QuinticEaseIn,
+
+        /// <summary>
+        ///     The quintic ease-out animation function.
+        /// </summary>
+        QuinticEaseOut,
+
+        /// <summary>
+        ///     The sinusoidal ease-in animation function.
+        /// </summary>
+        SinusoidalEaseIn,
+
+        /// <summary>
+        ///     The sinusoidal ease-out animation function.
+        /// </summary>
+        SinusoidalEaseOut,
+
+        /// <summary>
+        ///     The sinusoidal ease-in and ease-out animation function.
+        /// </summary>
+        SinusoidalEaseInOut,
+
+        /// <summary>
+        ///     The exponential ease-in animation function.
+        /// </summary>
+        ExponentialEaseIn,
+
+        /// <summary>
+        ///     The exponential ease-out animation function.
+        /// </summary>
+        ExponentialEaseOut,
+
+        /// <summary>
+        ///     The exponential ease-in and ease-out animation function.
+        /// </summary>
+        ExponentialEaseInOut
+    }
+}
