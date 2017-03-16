7041597d8df27f97db99463bbd36f19d8e091158
s_falahati <s_falahati@hotmail.com>
2017-03-16 19:11:42 +0330

    Fixed a problem with with DLL detected as a satellite assembly

--- WinFormAnimation/Properties/AssemblyInfo.cs
+++ WinFormAnimation/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("WinForm Animation Library")]
 [assembly: AssemblyCopyright("Soroush Falahati")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
